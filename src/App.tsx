835dbd9215b1c5812b45b4481843455bf5eb93ab
Guilherme Bim <guilherbim@hotmail.com>
2025-12-19 14:01:11 -0300

    Fix main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+
 import Index from "./pages/Index";
 import DtaToCsv from "./pages/DtaToCsv";
 import TxtToCsv from "./pages/TxtToCsv";
@@ -35,32 +36,32 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/dta-to-csv" element={<DtaToCsv />} />
-            <Route path="/txt-to-csv" element={<TxtToCsv />} />
-            <Route path="/csv-to-txt" element={<CsvToTxt />} />
-            <Route path="/csv-to-json" element={<CsvToJson />} />
-            <Route path="/json-to-csv" element={<JsonToCsv />} />
-            <Route path="/csv-to-xml" element={<CsvToXml />} />
-            <Route path="/xml-to-csv" element={<XmlToCsv />} />
-            <Route path="/xml-to-json" element={<XmlToJson />} />
-            <Route path="/csv-to-html" element={<CsvToHtml />} />
-            <Route path="/csv-to-sql" element={<CsvToSql />} />
-            <Route path="/csv-to-markdown" element={<CsvToMarkdown />} />
-            <Route path="/html-to-txt" element={<HtmlToTxt />} />
-            <Route path="/html-to-markdown" element={<HtmlToMarkdown />} />
-            <Route path="/txt-to-json" element={<TxtToJson />} />
-            <Route path="/txt-to-xml" element={<TxtToXml />} />
-            <Route path="/json-to-txt" element={<JsonToTxt />} />
-            <Route path="/json-to-xml" element={<JsonToXml />} />
-            <Route path="/json-to-yaml" element={<JsonToYaml />} />
-            <Route path="/extract-metadata" element={<ExtractMetadata />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/dta-to-csv" element={<DtaToCsv />} />
+          <Route path="/txt-to-csv" element={<TxtToCsv />} />
+          <Route path="/csv-to-txt" element={<CsvToTxt />} />
+          <Route path="/csv-to-json" element={<CsvToJson />} />
+          <Route path="/json-to-csv" element={<JsonToCsv />} />
+          <Route path="/csv-to-xml" element={<CsvToXml />} />
+          <Route path="/xml-to-csv" element={<XmlToCsv />} />
+          <Route path="/xml-to-json" element={<XmlToJson />} />
+          <Route path="/csv-to-html" element={<CsvToHtml />} />
+          <Route path="/csv-to-sql" element={<CsvToSql />} />
+          <Route path="/csv-to-markdown" element={<CsvToMarkdown />} />
+          <Route path="/html-to-txt" element={<HtmlToTxt />} />
+          <Route path="/html-to-markdown" element={<HtmlToMarkdown />} />
+          <Route path="/txt-to-json" element={<TxtToJson />} />
+          <Route path="/txt-to-xml" element={<TxtToXml />} />
+          <Route path="/json-to-txt" element={<JsonToTxt />} />
+          <Route path="/json-to-xml" element={<JsonToXml />} />
+          <Route path="/json-to-yaml" element={<JsonToYaml />} />
+          <Route path="/extract-metadata" element={<ExtractMetadata />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
       </TooltipProvider>
     </LanguageProvider>
   </QueryClientProvider>
